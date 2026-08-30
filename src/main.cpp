aab198d3351bbc0d69465104f890f050daa684d4
ProPlat28 <johnwickthebest96@gmail.com>
2026-08-30 15:58:17 +0200

    Fix formatting of zeroSprite creation

--- src/main.cpp
+++ src/main.cpp
@@ -547,7 +547,7 @@ class $modify(RSLHook, RateStarsLayer) {
             m_fields->m_coinBtn = coinBtn;
             m_fields->m_coinSprite = coinSprite;
 
-            auto zeroSprite = ButtonSprite::create("0", 36, false, "bigFont.fnt", "GJ_button_04.png", 30.f, 0.5.f);
+            auto zeroSprite = ButtonSprite::create("0", 36, false, "bigFont.fnt", "GJ_button_04.png", 30.f, 0.5f);
             zeroSprite->setScale(0.7f);
 
             auto zeroBtn = CCMenuItemSpriteExtra::create(
